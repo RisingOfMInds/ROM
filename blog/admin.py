f7636e221e9fde27c8db56f33b56a65775b37cbb
Ashutosh Singh <ashutosh.singh5335@gmail.com>
2019-02-03 13:30:54 +0530

    Comments integrated

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import AbstractUser
+from django.utils.safestring import mark_safe
 from markdownx.admin import MarkdownxModelAdmin
 
 from .models import Blog, Category, Images, Author
@@ -27,15 +29,31 @@ class BlogAdmin(MarkdownxModelAdmin):
         obj.save()
 
 
-class AuthorAdmin(UserAdmin):
-    model = Author
-    extra = 0
+class AuthorAdmin(admin.ModelAdmin):
 
     def get_queryset(self, request):
         qs = super(AuthorAdmin, self).get_queryset(request)
         if request.user.is_superuser:
             return qs
-        return qs.filter(id=request.user)
+        return qs.filter(id=request.user.id)
+
+    def get_exclude(self, request, obj=None):
+        excluded = super().get_exclude(request, obj) or []  # get overall excluded fields
+
+        if not request.user.is_superuser:  # if user is not a superuser
+            return excluded + ['last_login', 'is_superuser', 'is_staff', 'date_joined', 'user_permissions', 'groups',
+                               'is_active']
+
+        return excluded
+
+    def save_model(self, request, obj, form, change):
+        if obj.pk:
+            orig_obj = Author.objects.get(pk=obj.pk)
+            if obj.password != orig_obj.password:
+                obj.set_password(obj.password)
+        else:
+            obj.set_password(obj.password)
+        obj.save()
 
 
 admin.site.register(Blog, BlogAdmin)
